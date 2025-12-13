af985d2fdf003b1f6901351459659302c0421efe
Mahmoud200 <mahmoudjamal294@gmail.com>
2025-12-13 05:17:57 +0200

    Working in addition in student

--- JS/Add_Student.js
+++ JS/Add_Student.js
@@ -6,8 +6,20 @@ function addition(name , id , level ,major){
         alert("One Filed may empty at least !!!");
     }
     else{
-        alert("Nice!!")
-    }
+         const students = JSON.parse(localStorage.getItem('students') || '[]');
+
+    students.push({
+        name: name.value,
+        id: id.value,
+        level: level.value,
+        major: major.value
+    });
+
+    localStorage.setItem('students', JSON.stringify(students));
+    alert("Student Added Successfully !!");
+
+    window.location.href = "../pages/Student_Management.html";
+}
 }
 
 
@@ -24,4 +36,6 @@ const image = 0;
  const save = document.querySelector('#save');   
 
 
- save.addEventListener("click", ()=>addition(kName,kId,kLevel,KMajor));
+ save.addEventListener("click", (e)=>{
+    e.preventDefault();
+    addition(kName,kId,kLevel,KMajor)});
--- JS/Student_Management.js
+++ JS/Student_Management.js
@@ -38,3 +38,48 @@ editButtons.forEach(function (button) {
 const deleteButtons = document.querySelectorAll('#delete');
 
 deleteButtons.forEach(deleteSTD);
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const studentsList = document.querySelector('.students-list');
+
+    const students = JSON.parse(localStorage.getItem('students') || '[]');
+
+    students.forEach(student => {
+        const card = document.createElement('div');
+        card.classList.add('student-card');
+        card.innerHTML = `
+            <div class="card-top">
+                <div class="student-info">
+                    <div class="avatar-circle">
+                        <i class="fa-solid fa-user"></i>
+                    </div>
+                    <div class="info-text">
+                        <h3>${student.name}</h3>
+                        <span class="student-id">ID : ${student.id}</span>
+                    </div>
+                </div>
+                <div class="card-actions">
+                    <i class="fa-solid fa-pen icon-edit" id="edit" ></i>
+                    <i class="fa-solid fa-trash icon-delete" id="edit" ></i>
+                </div>
+            </div>
+            <div class="card-divider"></div>
+            <div class="card-details">
+                <div class="detail-item">
+                    <i class="fa-solid fa-graduation-cap"></i>
+                    <span>${student.major}</span>
+                </div>
+                <div class="detail-item">
+                    <i class="fa-regular fa-envelope"></i>
+                    <span>${student.name.replace(/\s+/g, '')}@university.edu</span>
+                </div>
+                <div class="detail-item">
+                    <i class="fa-solid fa-circle-notch"></i>
+                    <span>${student.level}</span>
+                </div>
+            </div>
+        `;
+        studentsList.appendChild(card);
+    });
+});
