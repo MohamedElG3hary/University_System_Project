05c752c84e1930ea72d0b9aa980a64308b84623a
Mahmoud200 <mahmoudjamal294@gmail.com>
2025-12-13 03:42:32 +0200

    Mahmoud Changes

---
+++ JS/Student_Management.js
@@ -0,0 +1,40 @@
+function add() {
+    alert("Add New Student clicked!");
+}
+function edit(btn) {
+    const studentCard = btn.closest(".student-card");
+    const studentIdText = studentCard.querySelector(".student-id").textContent;
+    const studentId = studentIdText.replace("ID : ", "").trim();
+
+    alert("Edit Student ID: " + studentId);
+}
+
+function msgDelete(btn) {
+    const studentCard = btn.closest(".student-card");
+    const studentIdText = studentCard.querySelector(".student-id").textContent;
+    const studentId = studentIdText.replace("ID : ", "").trim();
+
+    const confirmDelete = confirm("Do you want to delete student ID: " + studentId + " ?");
+    if (confirmDelete) {
+        alert("Student Deleted: " + studentId);
+        studentCard.remove();
+    }
+}
+
+function deleteSTD(button) {
+    button.addEventListener("click", () => msgDelete(button));
+}
+
+const addBtn = document.querySelector("#addbtn");
+if (addBtn) {
+    addBtn.addEventListener("click", () => add());
+}
+
+const editButtons = document.querySelectorAll('#edit'); 
+editButtons.forEach(function (button) {
+    button.addEventListener("click", () => edit(button));
+});
+
+const deleteButtons = document.querySelectorAll('#delete');
+
+deleteButtons.forEach(deleteSTD);
